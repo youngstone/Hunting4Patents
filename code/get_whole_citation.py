85a8d84d3132c46930f0ab42f42af007d5b6a93f
Stone Young <atombo@gmail.com>
2015-03-10 16:56:38 -0700

    add citation category

--- code/get_whole_citation.py
+++ code/get_whole_citation.py
@@ -6,8 +6,8 @@ import sqlite3
 
 def load_citation_database():
 	con = sqlite3.connect("../database/citation.sqlite3")
-	df = pd.read_sql("SELECT patent, citation, strftime('%Y', Cit_Date) AS year FROM citation", con)
-	df.to_csv('../data/full_citation_with_year.csv', index=False)
+	df = pd.read_sql("SELECT patent, citation, category FROM citation", con)
+	df.to_csv('../data/full_citation_with_category.csv', index=False)
 
 
 if __name__ == '__main__':
