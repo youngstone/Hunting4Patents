912649158b293faedd0b9d7a0bc3859a8e122575
Stone Young <atombo@gmail.com>
2015-03-11 12:21:56 -0700

    export pagerank csv

--- code/get_whole_citation.py
+++ code/get_whole_citation.py
@@ -4,11 +4,20 @@ import pandas as pd
 import sqlite3
 
 
-def load_citation_database():
+def export_full_citation_with_category():
 	con = sqlite3.connect("../database/citation.sqlite3")
 	df = pd.read_sql("SELECT patent, citation, category FROM citation", con)
 	df.to_csv('../data/full_citation_with_category.csv', index=False)
 
 
+def export_full_citation():
+	con = sqlite3.connect("../database/citation.sqlite3")
+	df = pd.read_sql("SELECT patent, citation FROM citation", con)
+	df.to_csv('../data/full_citation.csv', index=False)
+
+
 if __name__ == '__main__':
-	load_citation_database()
+	
+	# export_full_citation()
+
+	export_full_citation_with_category()
